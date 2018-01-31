fa2f519f02352ddeec7dfdb3597458c74e1f2870
Bryan Yu <bryanyu@mwc-122164.dhcp.missouri.edu>
2018-01-31 14:28:46 -0600

    First commit

---
+++ Swift Language Basics/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Swift Language Basics
+//
+//  Created by Bryan Yu on 1/31/18.
+//  Copyright © 2018 Bryan Yu. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
